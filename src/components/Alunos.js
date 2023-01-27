5ed972657ba077593075b33fba568f46e07009e3
heloysa <heloysagama2@gmail.com>
2023-01-27 14:09:32 -0300

    components

--- src/components/Alunos.js
+++ src/components/Alunos.js
@@ -1,7 +1,8 @@
 import React from 'react'
 import Table from 'react-bootstrap/Table';
 import { Button } from 'react-bootstrap';
-
+import Form from 'react-bootstrap/Form';
+import { Modal } from 'react-bootstrap';
   /*
     Como criar um construtor pra receber os dados da API
 
@@ -12,46 +13,203 @@ import { Button } from 'react-bootstrap';
 
  class Alunos extends React.Component{
 
+  //CONSTRUCTOR DO JSON
   constructor(props){
     super(props);  
     
     this.state = {
+      id: 0,
+      name: "",
+      email: "",
+      course: "",
       alunos : []
     }
   }
 
+//COMPONENTDIDMOUNT QUE SERVE PRA QUANDO A ABA DO MENU É APERTA
     componentDidMount(){
       this.buscarAluno();
     }
 
-    componentWillUnmount(){
-        
-    }
 
+
+    //Get
     buscarAluno = () => {
-      fetch('http://localhost:3030/api/users/aluno').then(resposta => resposta.json()).then(dados => {
-        this.setState({ alunos: dados})
+      fetch('http://localhost:3030/aluno').then(resposta => resposta.json()).then(aluno => {
+        this.setState({ alunos: aluno})
       })
     }
 
+    //DELETE
     deletarAluno = (id) => {
       fetch('http://localhost:3030/'+id, {
         method: 'DELETE'
       }).then(resposta => {
         if(resposta.ok){
           this.buscarAluno();
+        }else{
+          alert('Não foi possivel adicionar o aluno')
         }
       })
     }
 
+    //GET (ALUNO COM UM ID)
+    carregarDados = (id) => {
+      fetch('http://localhost:3030/aluno/' +id, {
+        method: 'GET'
+      }).then(resposta => resposta.json()).then(aluno => {
+        this.setState({ 
+          aluno: aluno.id,
+          name: aluno.name,
+          email: aluno.email
+        })
+      })
+    }
+
+    // POST ALUNO
+    cadastraAluno = (aluno) => {
+      fetch('http://localhost:3030/cadastro', {
+        method: 'POST'
+      , headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify(aluno)
+    }
+      ).then(resposta => {
+        if(resposta.ok){
+          this.buscarAluno();
+        }
+      })
+    }
+
+    //PUT
+    atualizarAluno = (aluno) => {
+      fetch('http://localhost:3030/atualizar'+aluno.id, {
+        method: 'PUT'
+      , headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify(aluno)
+    }
+      ).then(resposta => {
+        if(resposta.ok){
+          this.buscarAluno();
+        } else{
+          alert(JSON.stringify(resposta))
+        }
+      })
+    }
+
+
+    // ENVIANDO OS DADOS PRO POST
+
+    atualizaNome = (e) => {
+      this.setState(
+        {
+           name: e.target.value,
+        }
+      )
+    }
+    
+    atualizaEmail = (e) => {
+      this.setState(
+        {
+           email: e.target.value,
+        }
+      )
+    }
+    
+    atualizaCurso = (e) => {
+      this.setState(
+        {
+           course: e.target.value,
+        }
+      )
+    }
+
+    //
+
+    // SUBMIT PRO POST
+    submit = () => {
+      if(this.state.id === 0){
+        const aluno = {
+          name: this.state.name,
+          email: this.state.email,
+          course: this.state.course,
+        }
+        this.cadastraAluno(aluno)
+      
+      } else {
+        const aluno = {
+          id: this.state.id,
+          email: this.state.email,
+          course: this.state.course,
+        }
+        this.atualizarAluno(aluno)
+      }
+    }
+    
+
+    
+
+    // RENDERS
+
+    renderModal(){
+      return(
+        <Modal>
+        <Form>
+        <Form.Group className="m-3" >
+          <Form.Label>ID</Form.Label>
+          <Form.Control  value={this.state.id} readOnly={true}/>
+        </Form.Group>
+        <Form.Group className="m-3" >
+          <Form.Label>Nome</Form.Label>
+          <Form.Control  value={this.state.name} readOnly={true}/>
+        </Form.Group>
+        <Form.Group className="m-3" >
+          <Form.Label>Email</Form.Label>
+          <Form.Control type="email" placeholder="Insira seu email" value={this.state.email} onChange={this.atualizaEmail} />
+        </Form.Group>
+        <Form.Group className="m-3" >
+          <Form.Label>Curso</Form.Label>
+          <Form.Control type="text" placeholder="Insira seu curso" value={this.state.course}  onChange={this.atualizaCurso} />
+        </Form.Group>
+        <Button variant="primary" type="submit" className='m-3' onClick={this.submit} >
+          Salvar
+        </Button>
+      </Form>
+      </Modal>
+      )  
+    }
+
+    renderFormulario(){
+      return(
+        <Form>
+        <Form.Group className="m-3" >
+          <Form.Label>Nome</Form.Label>
+          <Form.Control type="name" placeholder="Insira seu nome" value={this.state.name} onChange={this.atualizaNome}/>
+        </Form.Group>
+        <Form.Group className="m-3" >
+          <Form.Label>Email</Form.Label>
+          <Form.Control type="email" placeholder="Insira seu email" value={this.state.email} onChange={this.atualizaEmail} />
+        </Form.Group>
+        <Form.Group className="m-3" >
+          <Form.Label>Curso</Form.Label>
+          <Form.Control type="text" placeholder="Insira seu curso" value={this.state.course}  onChange={this.atualizaCurso} />
+        </Form.Group>
+        <Button variant="primary" type="submit" className='m-3' onClick={this.submit} >
+          Cadastrar
+        </Button>
+      </Form>
+
+      );
+    }
+    
   
     renderTabela() {
       return (
-        <Table striped bordered hover>
+        <Table striped bordered hover className='m-3'>
           <thead>
             <tr>
               <th>Nome</th>
               <th>Email</th>
+              <th>Curso</th>
               <th>Opções</th>
             </tr>
           </thead>
@@ -60,9 +218,10 @@ import { Button } from 'react-bootstrap';
               <tr key={aluno.id}>
                 <td>{aluno.name}</td>
                 <td>{aluno.email}</td>
+                <td>{aluno.course}</td>
                 <td>
                   <div>
-                    <Button variant="">Atualizar</Button>
+                  <Button variant="secondary" onClick={() => this.carregarDados(aluno.id)}>Atualizar</Button>
                     <Button variant="danger" onClick={() => this.deletarAluno(aluno.id)}>Excluir</Button>
                   </div>
                 </td>
@@ -73,12 +232,16 @@ import { Button } from 'react-bootstrap';
       );
     }
   
+
   
  
+    // RENDER PRINCIPAL
     render() {
       return (
         <div>
           <br />
+          {this.renderModal()}
+          {this.renderFormulario()}
           {this.renderTabela()}
         </div>
       );
--- src/components/Sobre.js
+++ src/components/Sobre.js
@@ -8,6 +8,17 @@
 //   )
 // }
 
+function Sobre(){
+  return(
+    <div>
+      <p>Sobre</p>
+    </div>
+  )
+}
+export default Sobre;
+
+
+
 // export default Sobre;
 // function Alunos(){ 
 //   const cont = 0;
